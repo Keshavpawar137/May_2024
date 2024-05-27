2767ccbd9b890f023b0e447e7142e3589cee040d
Keshavpawar137 <keshavpawar3333@gmail.com>
2024-05-27 17:55:35 +0530

    Add files via upload

---
+++ Descriptive_Stat.R
@@ -0,0 +1,47 @@
+12345
+
+8+85
+54877*43
+
+number =54877*43
+number+2
+age =21
+
+class(age)
+
+weight = 58.2
+class(weight)
+
+name ='Keshav'
+class(name)
+
+bool = TRUE
+class(bool)
+
+x=F
+class(x)
+
+name2 ='Keshav'
+class(name2)
+
+
+#age = 21,25,26
+
+age= c(21,22,23,24,25)
+
+View(data)
+View(temp)
+View(bank)
+
+xyz = datasets::AirPassengers
+abc = datasets::airmiles
+rm(xyz)
+rm(data)
+
+
+
+
+
+
+
+
